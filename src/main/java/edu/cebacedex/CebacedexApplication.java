ac2828e9fd05e75db473a679fc92c5a8c17107ae
edgarciaguilar <edgarciaguilar@gmail.com>
2023-08-08 07:28:31 -0600

    Versión inicial del proyecto CEBA CEDEX

---
+++ src/main/java/edu/cebacedex/CebacedexApplication.java
@@ -0,0 +1,13 @@
+package edu.cebacedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CebacedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CebacedexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/cebacedex/CebacedexApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cebacedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CebacedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
